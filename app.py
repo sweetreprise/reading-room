07a7907d35a94dcf1feb6cd5d6ea854a80481cda
maria acuna <taeny90@gmail.com>
2022-01-21 20:26:36 -0500

    attempt to fix database issues

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from forms import LoginForm, RegisterForm, UserEditForm
 # CURR_USER_KEY = "current_user"
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require','postgresql:///reading_room').replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_AQUA_URL','postgresql:///reading_room').replace('postgres://', 'postgresql://', 1)
 
 
 
